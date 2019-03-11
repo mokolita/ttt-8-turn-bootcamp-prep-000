2c52b5859d366413a3cc7b04f10430af81e9a908
Meg Galvez <meg@oakmaine.com>
2019-03-11 15:29:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,6 +36,9 @@ def turn(board)
  index = input_to_index(user_input)
    
   if valid_move?(board, index)
-    
+    move(board, index, character = "X")
+    display_board(board)
+  else turn(board)
+  end 
 
 end 
